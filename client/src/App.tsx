8099a26acb2d2c5139a9be05d833bce1d6daed0b
bt1k <6884455+bt1k@users.noreply.github.com>
2025-12-19 21:33:16 +0000

    Move <main> content into Main component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,37 +1,7 @@
-import { useEffect, useState } from 'react';
+import Main from './components/Main';
 import './App.css';
 
-export function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [hasError, setHasError] = useState(false);
-  const [posts, setPosts] = useState<Post[]>([]);
-
-  useEffect(() => {
-    getPosts();
-  }, []);
-
-  async function getPosts() {
-    setIsLoading(true);
-    setHasError(false);
-    setPosts([]);
-    try {
-      // TODO: Remove simulated latency. Just for testing at the moment.
-      await new Promise((resolve) => setTimeout(resolve, 1_000));
-      const response = await fetch('http://localhost:3000/api/v1/posts');
-      if (!response.ok) throw new Error('Fetching posts failed');
-      const json: PostDTO[] = await response.json();
-      setPosts(
-        json.map((postDTO) => ({
-          ...postDTO,
-          createdAt: new Date(postDTO.createdAt),
-        })),
-      );
-    } catch {
-      setHasError(true);
-    }
-    setIsLoading(false);
-  }
-
+export default function App() {
   return (
     <>
       <header id="appHeader">
@@ -43,29 +13,7 @@ export function App() {
       </header>
       <div id="appContainer">
         <main id="appMain">
-          {isLoading ? (
-            <p>Loading...</p>
-          ) : (
-            <>
-              <p>
-                <button onClick={getPosts}>Reload</button>
-              </p>
-              {hasError ? (
-                <>
-                  <p>Error!</p>
-                </>
-              ) : (
-                <>
-                  <p>{posts.length} post(s) loaded.</p>
-                  <ol>
-                    {posts.map((post) => (
-                      <li key={post.id}>{JSON.stringify(post)}</li>
-                    ))}
-                  </ol>
-                </>
-              )}
-            </>
-          )}
+          <Main />
         </main>
       </div>
     </>
---
+++ client/src/components/Main.tsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from 'react';
+
+export default function Main() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  async function getPosts() {
+    setIsLoading(true);
+    setHasError(false);
+    setPosts([]);
+    try {
+      // TODO: Remove simulated latency. Just for testing at the moment.
+      await new Promise((resolve) => setTimeout(resolve, 1_000));
+      const response = await fetch('http://localhost:3000/api/v1/posts');
+      if (!response.ok) throw new Error('Fetching posts failed');
+      const json: PostDTO[] = await response.json();
+      setPosts(
+        json.map((postDTO) => ({
+          ...postDTO,
+          createdAt: new Date(postDTO.createdAt),
+        })),
+      );
+    } catch {
+      setHasError(true);
+    }
+    setIsLoading(false);
+  }
+
+  if (isLoading) return <p>Loading...</p>;
+
+  return (
+    <>
+      <p>
+        <button onClick={getPosts}>Reload</button>
+      </p>
+      {hasError ? (
+        <p>Error!</p>
+      ) : (
+        <>
+          <p>{posts.length} post(s) loaded.</p>
+          <ol>
+            {posts.map((post) => (
+              <li key={post.id}>{JSON.stringify(post)}</li>
+            ))}
+          </ol>
+        </>
+      )}
+    </>
+  );
+}
