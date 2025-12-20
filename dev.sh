86029a7fb556a98dd194e01bbf4293b15e6278e5
bt1k <6884455+bt1k@users.noreply.github.com>
2025-12-20 15:54:23 +0000

    Add dev script
    This script is intended for running this project in development. The script opens up tmux and, within tmux, runs both the Go back-end and the React front-end (with Parcel).

---
+++ dev.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# This script starts the back-end and front-end in development.
+# You must install the following for this script to work:
+# * tmux
+# * Node.js (version 24 or later recommended; earlier versions may also work)
+# * Go (I think any version should be okay because when `go run` is run, I think
+#   it will download the Go version specified in the file `server/go.mod`)
+
+SESSION_NAME="emojiboard"
+
+if tmux has-session -t $SESSION_NAME 2>/dev/null; then
+  echo "Session $SESSION_NAME exists; attaching to it"
+else
+  tmux new-session -d -s $SESSION_NAME
+  tmux split-window -v
+  tmux send-keys -t 0 "cd server" C-m
+  tmux send-keys -t 0 "go run ." C-m
+  tmux send-keys -t 1 "cd client" C-m
+  tmux send-keys -t 1 "npm run dev" C-m
+fi
+
+tmux attach-session -t $SESSION_NAME
