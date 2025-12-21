296243039ca1d8cef6b4fea19447d26d0ee53051
bt1k <6884455+bt1k@users.noreply.github.com>
2025-12-21 21:10:29 +0000

    Add React Compiler
    This appears to be successfully memoising each <Post> component automatically, and therefore each <Post> is no longer re-rendering when `secondsElapsed` is updated in PostsList.tsx.

--- client/eslint.config.ts
+++ client/eslint.config.ts
@@ -6,5 +6,7 @@ import tseslint from 'typescript-eslint';
 export default defineConfig([
   eslint.configs.recommended,
   tseslint.configs.recommended,
-  reactHooks.configs.flat.recommended,
+  // The `recommended-latest` rules have rules for the React Compiler, which is
+  // enabled in this project.
+  reactHooks.configs.flat['recommended-latest'],
 ]);
