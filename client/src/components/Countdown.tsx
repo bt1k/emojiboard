9f298a1b937ec0eba7db11267d69b69a0727232c
bt1k <6884455+bt1k@users.noreply.github.com>
2025-12-22 11:35:26 +0000

    Add possibly unnecessary optimisation
    In the last commit, PostsList was re-rendering every second. I have refactored the code so that now it seems only the Countdown component is re-rendering every second. I tested the performance with the React Profiler (part of the React Developer Tools browser extension). Before, each render seemed to take at least 1.4 ms. Now, each render seems to take about 0.4 - 0.6 ms. Perhaps this optimisation is small and unnecessary, but hopefully it makes this website a little more efficient.
    
    I have replaced `setInterval` with `setTimeout`. I don't think there is much point to `setInterval` in this new code, because the effect has to run every second anyway, because it is dependent upon the value of `secondsElapsed`. I tried getting the `secondsElapsed` value within the setter, `setSecondsElapsed`, but then you need to set `dateUpdatedEveryMinute` within the `setSecondsElapsed` function, and React doesn't seem to like that - it will show this error:
    
    "Cannot update a component (`PostsList`) while rendering a different component (`Countdown`). To locate the bad setState() call inside `Countdown`, follow the stack trace as described in https://react.dev/link/setstate-in-render"

--- client/src/components/Countdown.tsx
+++ client/src/components/Countdown.tsx
@@ -1,7 +1,29 @@
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+
 type CountdownProps = {
-  secondsElapsed: number;
+  setDateUpdatedEveryMinute: Dispatch<SetStateAction<Date>>;
 };
 
-export default function Countdown({ secondsElapsed }: CountdownProps) {
+export default function Countdown({
+  setDateUpdatedEveryMinute,
+}: CountdownProps) {
+  const [secondsElapsed, setSecondsElapsed] = useState(0);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      // If `secondsElapsed` is 59 and it's supposed to be updated, then it's
+      // supposed to be updated to 60 - or rather it is supposed to be reset to
+      // 0. On a watch, the number 60 for seconds is never shown - instead it
+      // just goes straight to 0.
+      if (secondsElapsed < 59) {
+        setSecondsElapsed((secs) => secs + 1);
+      } else {
+        setSecondsElapsed(0);
+        setDateUpdatedEveryMinute(new Date());
+      }
+    }, 1_000);
+    return () => clearTimeout(timeoutId);
+  }, [secondsElapsed, setDateUpdatedEveryMinute]);
+
   return <>{59 - secondsElapsed}</>;
 }
--- client/src/components/PostsList.tsx
+++ client/src/components/PostsList.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Countdown from './Countdown';
 import Post from './Post';
 
@@ -13,28 +13,9 @@ export default function PostsList({
   hasError,
   posts,
 }: PostsListProps) {
-  const [secondsElapsed, setSecondsElapsed] = useState(0);
   const [dateUpdatedEveryMinute, setDateUpdatedEveryMinute] = useState(
     new Date(),
   );
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setSecondsElapsed((secs) => {
-        // If `secondsElapsed` is 59 and it's supposed to be updated, then it's
-        // supposed to be updated to 60 - or rather it is supposed to be reset
-        // to 0. On a watch, the number 60 for seconds is never shown - instead
-        // it just goes straight to 0.
-        if (secs < 59) {
-          return secs + 1;
-        } else {
-          setDateUpdatedEveryMinute(new Date());
-          return 0;
-        }
-      });
-    }, 1_000);
-    return () => clearInterval(intervalId);
-  }, []);
 
   return (
     <>
@@ -42,7 +23,8 @@ export default function PostsList({
       {posts.length > 0 && (
         <p>
           The age of each post is updated every minute (will update in{' '}
-          <Countdown secondsElapsed={secondsElapsed} /> second(s)).
+          <Countdown setDateUpdatedEveryMinute={setDateUpdatedEveryMinute} />{' '}
+          second(s)).
         </p>
       )}
       <p>
